53b8b8c48f69217301a6c92aa1e23cf44331196f
Richard Wang <slytherinheir1214@gmail.com>
2024-09-30 21:51:09 -0400

    Change experimental_solver to solver

--- .github/scripts/install-conda.sh
+++ .github/scripts/install-conda.sh
@@ -166,7 +166,7 @@ else
     $SUDO "$CONDA_EXE" install $DRY_RUN_OPTION -y -n base conda-libmamba-solver
 
     # Use the fast solver by default
-    "${DRY_RUN_ECHO[@]}" $SUDO "$CONDA_EXE" config --system --set experimental_solver libmamba
+    "${DRY_RUN_ECHO[@]}" $SUDO "$CONDA_EXE" config --system --set solver libmamba
 
     # conda-lock is a special case and must always be installed into the base environment
     $SUDO "$CONDA_EXE" install $DRY_RUN_OPTION -y -n base conda-lock=1.4
